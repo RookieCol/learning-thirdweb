cf13e916b25d9520338a97489f11f4dc0df267a9
RookieCol <81944029+RookieCol@users.noreply.github.com>
2024-08-01 07:35:19 +0000

    jsdoc for current fucntions

--- index.ts
+++ index.ts
@@ -11,16 +11,43 @@ if (!clientId || !privateKey) {
   throw Error("Missing environment variables");
 }
 
+/**
+ * Create a Thirdweb client instance.
+ * @param {Object} config - The configuration object for creating the client.
+ * @param {string} config.clientId - The client ID used for authenticating with Thirdweb.
+ * @returns {Object} - The Thirdweb client instance.
+ */
 const client = createThirdwebClient({
   clientId,
 });
 
+/**
+ * Converts a private key to an account object.
+ * @param {Object} config - The configuration object for the account.
+ * @param {string} config.privateKey - The private key to convert into an account.
+ * @param {Object} config.client - The Thirdweb client instance.
+ * @returns {Object} - The account object containing address and key information.
+ */
 const account = privateKeyToAccount({
   privateKey,
   client,
 });
 
 try {
+  /**
+   * Deploy an ERC20 contract to a specified blockchain network.
+   * @async
+   * @param {Object} config - The deployment configuration object.
+   * @param {Object} config.chain - The blockchain network where the contract will be deployed.
+   * @param {Object} config.client - The Thirdweb client instance.
+   * @param {Object} config.account - The account used to deploy the contract.
+   * @param {string} config.type - The type of contract to deploy (e.g., "TokenERC20").
+   * @param {Object} config.params - The parameters for the contract deployment.
+   * @param {string} config.params.name - The name of the ERC20 token.
+   * @param {string} config.params.description - The description of the token contract.
+   * @param {string} config.params.symbol - The symbol of the ERC20 token.
+   * @returns {Promise<string>} - The address of the deployed contract.
+   */
   const contractAddress = await deployERC20Contract({
     chain: arbitrumSepolia,
     client,
